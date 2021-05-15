bd6c55f12aea059037a779478695706adb8aaae1
Alexis Smith <alexis@nextlevelurgentcare.com>
2021-05-15 12:45:37 -0500

    checkins

--- checkins.js
+++ checkins.js
@@ -2,3 +2,8 @@ function changeColor() {
     const color = document.getElementById("colors").value;
     document.body.style.backgroundColor = color;
   }
+
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
